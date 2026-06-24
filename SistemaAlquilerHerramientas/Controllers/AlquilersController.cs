171c94c4e7a8f879254f96d0e319e24ebfff6b1c
Adriano0211 <202310536@urp.edu.pe>
2026-06-24 00:41:56 -0500

    Cambio final

--- SistemaAlquilerHerramientas/Controllers/AlquilersController.cs
+++ SistemaAlquilerHerramientas/Controllers/AlquilersController.cs
@@ -84,7 +84,7 @@ namespace SistemaAlquilerHerramientas.Controllers
                     "La fecha de devolución pactada debe ser posterior a la fecha de entrega.");
             }
 
-            // Si hay errores -> recargar selects y volver a la vista
+            // Si hay errores - recargar selects y volver a la vista
             if (!ModelState.IsValid)
             {
                 ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "Nombres", alquiler.IdCliente);
@@ -93,7 +93,7 @@ namespace SistemaAlquilerHerramientas.Controllers
                 return View(alquiler);
             }
 
-            // Todo válido -> calcular monto y guardar
+            // Todo válido - calcular monto y guardar
             var herramienta = await _context.Herramienta.FindAsync(alquiler.IdHerramienta);
             int dias = (alquiler.FechaDevolucionPactada - alquiler.FechaEntrega).Days;
             alquiler.MontoEstimado = herramienta!.PrecioPorDia * dias;
@@ -103,11 +103,11 @@ namespace SistemaAlquilerHerramientas.Controllers
             _context.Alquilers.Add(alquiler);
             await _context.SaveChangesAsync();
 
-            // RN-09: cambiar estado herramienta -> Alquilada
+            // RN-09: cambiar estado herramienta - Alquilada
             _estadoService.CambiarEstado(alquiler.IdHerramienta,
                 HerramientaEstadoService.Estados.Alquilada);
 
-            // Si venía de una reserva, cerrar
+            // Si venía de una reserva, cerrarla
             if (alquiler.IdReserva.HasValue)
             {
                 var reserva = await _context.Reservas.FindAsync(alquiler.IdReserva.Value);
